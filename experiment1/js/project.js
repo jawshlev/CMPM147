a9a7d9a92a76eb94eae6b2bbd6790904e01de620
josh lev <soybr3ad@gmail.com>
2024-04-08 14:13:01 -0700

    removing excessive comments

--- experiment1/js/project.js
+++ experiment1/js/project.js
@@ -5,23 +5,6 @@
 // NOTE: This is how we might start a basic JavaaScript OOP project
 
 // Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-
-// // define a class
-// class MyProjectClass {
-//   // constructor function
-//   constructor(param1, param2) {
-//     // set properties using 'this' keyword
-//     this.property1 = param1;
-//     this.property2 = param2;
-//   }
-  
-//   // define a method
-//   myMethod() {
-//     // code to run when method is called
-//   }
-// }
-
 function main() {
   const fillers = {
     Person: ["Bloke", "Dawgie", "Dude", "Bro", "Brosef", "Gurl", "Girlie Pop", "Miss Ma'am", "Bruh"],
